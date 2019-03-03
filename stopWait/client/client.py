7b0d156ae3fc259601c99aec536a3191eb89c1d0
krward2 <43589922+krward2@users.noreply.github.com>
2019-03-03 15:33:23 -0700

    Added acknowledgements, stop-and-wait, and server retransmission

--- stopWait/client/client.py
+++ stopWait/client/client.py
@@ -12,6 +12,7 @@ print('Requesting file: ' + fileName)
 requestedFile = open(fileName, 'w')
 _fileName= fileName.encode()
 clientSock.sendto(_fileName, serverAddr)
+expectedID = 1
 
 while 1:
     _packet = clientSock.recv(100)
@@ -19,9 +20,20 @@ while 1:
 
     #TODO: consider trying to remove padding in last packet
     (packetID, _fileContents) = struct.unpack('H98s', _packet)
+
+    #Stops and waits until expected packet is recieved
+    while packetID != expectedID:
+        _packet = clientSock.recv(100)
+        (packetID, _fileContents) = struct.unpack('H98s', _packet)
+
     fileContents = _fileContents.decode()
     print(fileContents)
     requestedFile.write(str(fileContents))
+
+    #Sends acknowledgement containing ID of recieved packet back to server
+    ACK = struct.pack('H', packetID)
+    clientSock.sendto(ACK, serverAddr)
+    expectedID += 1
 
 requestedFile.close()
 print(fileName + ' recieved.')
--- stopWait/server/server.py
+++ stopWait/server/server.py
@@ -18,10 +18,24 @@ timeouts = 0
 packetID = 1
 
 while 1:
+    #File contents read and encoded into UTF-8
     fileContents = file.read(98)
     _fileContents = fileContents.encode()
+
+    #PacketID and fileContents converted into bytes object and sent
     _packet = struct.pack('H98s', packetID, _fileContents)
     serverSock.sendto(_packet, clientAddr)
+
+    #Stops and waits for acknowledgement of packet just sent
+    #Resends packet if incorrect ID acknowledgement is recieved
+    while 1:
+        _ACK = serverSock.recv(2)
+        ACK = struct.unpack('H', _ACK)[0]
+        print(str(ACK))
+        print(str(packetID))
+        if ACK == packetID: break
+        serverSock.sendto(_packet, clientAddr)
+
     if not fileContents: break
     packetID += 1
 
