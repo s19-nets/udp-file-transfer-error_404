8fc1f1a8296a49a7e17032a52bcfd419bc179001
krward2 <43589922+krward2@users.noreply.github.com>
2019-03-03 13:25:06 -0700

    Added 2 byte ID for packets

--- stopWait/client/client.py
+++ stopWait/client/client.py
@@ -1,26 +1,27 @@
 #! /usr/bin/env python3
 
 from socket import *
-import sys, os, re, time
-
-ACK = 'ack'
-ACK = ACK.encode()
+import sys, os, re, time, struct
 
 clientSock = socket(AF_INET,SOCK_DGRAM)
 serverAddr = (('127.0.0.1',50000))
 
-fileName = input("Enter the file name in name.extension format.\n")
+#fileName = input("Enter the file name in name.extension format.\n")
+fileName = 'test.txt'
 print('Requesting file: ' + fileName)
 requestedFile = open(fileName, 'w')
 _fileName= fileName.encode()
-clientSock.sendto(_fileName,addr)
+clientSock.sendto(_fileName, serverAddr)
 
 while 1:
-    _fileContents = clientSock.recv(100)
-    clientSock.sendto(ACK, serverAddr)
+    _packet = clientSock.recv(100)
+    if not _packet: break
+
+    #TODO: consider trying to remove padding in last packet
+    (packetID, _fileContents) = struct.unpack('H98s', _packet)
     fileContents = _fileContents.decode()
-    if not fileContents: break
-    requestedFile.write(fileContents)
+    print(fileContents)
+    requestedFile.write(str(fileContents))
 
 requestedFile.close()
 print(fileName + ' recieved.')
--- stopWait/server/server.py
+++ stopWait/server/server.py
@@ -1,10 +1,7 @@
 #! /usr/bin/env python3
 
 from socket import *
-import sys, os, re, time
-
-eofToken = ''
-eofToken = eofToken.encode()
+import sys, os, re, time, struct
 
 serverSock = socket(AF_INET,SOCK_DGRAM)
 serverAddr = (('127.0.0.1',50000))
@@ -17,11 +14,21 @@ print('Client, ' + repr(clientAddr) + ' requesting ' + fileName)
 
 file = open(fileName, 'r')
 
+timeouts = 0
+packetID = 1
+
 while 1:
-    fileContents = file.read(100)
+    fileContents = file.read(98)
     _fileContents = fileContents.encode()
-    serverSock.sendto(_fileContents, clientAddr)
+    _packet = struct.pack('H98s', packetID, _fileContents)
+    serverSock.sendto(_packet, clientAddr)
     if not fileContents: break
+    packetID += 1
+
+#temporary eof token for recv() to stop
+eof = ''
+serverSock.sendto(eof.encode(), clientAddr)
+
 file.close()
 
 print('File sent')
