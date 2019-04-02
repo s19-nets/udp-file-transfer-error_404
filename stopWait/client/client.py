a708903803b901a3d4e7c8befefd91ada5eeb10d
krward2 <43589922+krward2@users.noreply.github.com>
2019-04-02 15:07:36 -0700

    Initial select driven version

--- stopWait/client/client.py
+++ stopWait/client/client.py
@@ -1,98 +1,40 @@
 #! /usr/bin/env python3
 
 from socket import *
+from select import select
 import sys, os, re, time, struct
 
 clientSock = socket(AF_INET,SOCK_DGRAM)
-clientSock.settimeout(8)
+clientSock.setblocking(False)
 serverAddr = (('127.0.0.1',50000))
+expectedID = 1
+timeout = 20
 
 #fileName = input("Enter the file name in name.extension format.\n")
 fileName = 'test.txt'
-print('Requesting file ' + fileName + '...')
+print('Requesting file: ' + fileName)
 requestedFile = open(fileName, 'w')
 _fileName= fileName.encode()
 clientSock.sendto(_fileName, serverAddr)
-expectedID = 1
-wrongPacket = 0
-timeout = 0
-
-#Ensures the initial request is recieved
-while 1:
-    try:
-
-        if timeout == 4:
-            print('Server unresponsive, terminating.')
-            sys.exit(1)
 
 
-        _packet = clientSock.recv(100)
-        (packetID, _fileContents) = struct.unpack('H98s', _packet)
+def processPacket():
+    global expectedID
 
-        #Terminates if server can't find requested fileName
-        if not packetID:
-            print('Requested file cannot be found, terminating.')
-            sys.exit(1)
-
-        #Decode and write text to file
-        fileContents = _fileContents.decode()
-        print(fileContents)
-        requestedFile.write(str(fileContents))
-
-        #Sends acknowledgement containing ID of recieved packet back to server
-        ACK = struct.pack('H', packetID)
-        clientSock.sendto(ACK, serverAddr)
-        expectedID += 1
-
-        break
-
-    except:
-        timeout += 1
-        print('No response from server, resending request')
-        clientSock.sendto(_fileName, serverAddr)
-
-
-while 1:
-    #Checks for timeouts
-    try:
-        _packet = clientSock.recv(100)
-    except:
-        print('Server unresponsive, terminating.')
-        sys.exit(1)
-
-    #Stops waiting for packets when empty string is recieved (EOF signal)
-    if not _packet: break
+    _packet = clientSock.recv(100)
+    if not _packet: closeFile()
 
     #TODO: consider trying to remove padding in last packet
     (packetID, _fileContents) = struct.unpack('H98s', _packet)
 
-    #Stops and waits until expected packet is recieved.
-    #Terminates after ten consecutive incorrect packets arrive, assumes network
-    #is unstable
+    print(str(expectedID))
+    print(str(packetID))
+
+    #Stops and waits until expected packet is recieved
     while packetID != expectedID:
-
-        #Acknowledge invalid packet but do not write to file, and
-        #wait for correct packet.
-        print('Wrong packet ID')
-        wrongPacket += 1
-        ACK = struct.pack('H', packetID)
-        clientSock.sendto(ACK, serverAddr)
-
-        try:
-            _packet = clientSock.recv(100)
-        except:
-            print('Server unresponsive, terminating.')
-            sys.exit(1)
-
+        _packet = clientSock.recv(100)
         (packetID, _fileContents) = struct.unpack('H98s', _packet)
-        if wrongPacket >=10:
-            print('Network malfunction, terminating.')
-            sys.exit(1)
 
-    #Reset counter
-    wrongPacket = 0
-
-    #Decode and write text to file
     fileContents = _fileContents.decode()
     print(fileContents)
     requestedFile.write(str(fileContents))
@@ -101,6 +43,30 @@ while 1:
     ACK = struct.pack('H', packetID)
     clientSock.sendto(ACK, serverAddr)
     expectedID += 1
+    print('client boop')
 
-requestedFile.close()
-print(fileName + ' recieved.')
+def closeFile():
+    requestedFile.close()
+    print(fileName + ' recieved.')
+    sys.exit(1)
+
+
+readSockFunc = {}
+writeSockFunc = {}
+errorSockFunc = {}
+
+readSockFunc[clientSock] = processPacket
+
+while 1:
+    readRdySet, writeRdySet, errorRdySet = select(list(readSockFunc.keys()),
+                                                list(writeSockFunc.keys()),
+                                                list(errorSockFunc.keys()),
+                                                timeout)
+
+    if not readRdySet and not writeRdySet and not errorRdySet:
+        print('Server unresponsive. Request terminated.')
+        sys.exit(1)
+
+    for sock in readRdySet:
+        print('Packet recieved')
+        readSockFunc[sock]()
--- stopWait/server/server.py
+++ stopWait/server/server.py
@@ -1,101 +1,103 @@
 #! /usr/bin/env python3
 
 from socket import *
+from select import select
 import sys, os, re, time, struct
 
 serverSock = socket(AF_INET,SOCK_DGRAM)
-serverSock.settimeout(2)
-serverAddr = (('127.0.0.1', 50001))
+serverAddr = (('127.0.0.1',50000))
 serverSock.bind(serverAddr)
+print('Waiting for request...')
+clientAddr = ''
+fileName = ''
+file = 0
+timeout = 3
 timeouts = 0
 packetID = 1
-#temporary eof/end-of-transmission token for clientSock.recv() to stop
-eof = ''
+_packet = ''
 
-print('Waiting for request...')
+def openFile(sock):
+    global clientAddr
+    global file
+    global fileName
 
-#Waiting for file request. Checks for timeouts, and
-#terminates on 4th.
-while 1:
-    try:
-        (_fileName, clientAddr) = serverSock.recvfrom(100)
-        timeouts = 0
-        break
-    except:
-        if timeouts >= 4:
-            print('No requests, terminating')
-            sys.exit(1)
-        print('Timeout, waiting for request...')
-        timeouts += 1
+    (_fileName, clientAddr) = sock.recvfrom(2048)
+    fileName = _fileName.decode()
+    print('Client, ' + repr(clientAddr) + ' requesting ' + repr(fileName))
+    file = open(fileName, 'r')
 
-fileName = _fileName.decode()
+def sendNextPacket(sock):
+    global packetID
+    global file
+    global clientAddr
 
-#Makes sure file exists. Terminates if not.
-if not os.path.isfile('./' + fileName):
-    print('Client requesting non-existant file, terminating.')
-    _packet = struct.pack('H98s', 0, eof.encode())
-    serverSock.sendto(_packet, clientAddr)
-    sys.exit(1)
-
-#Accesses requested file.
-print('Client, ' + repr(clientAddr) + ' requesting ' + fileName)
-file = open(fileName, 'r')
-
-#Sends packets and waits for acknowledgements until end of document is reached,
-#or unless 4 consecutive timeouts occur.
-while 1:
     #File contents read and encoded into UTF-8
     fileContents = file.read(98)
+    if not fileContents:
+        #temporary
+        eof = ''
+        sock.sendto(eof.encode(), clientAddr)
+        file.close()
+        print('File sent')
+        sys.exit(1)
     _fileContents = fileContents.encode()
 
-    #PacketID and fileContents converted into bytes object and sent
+    #PacketID and fileContents converted into bytes object and sent to client
     _packet = struct.pack('H98s', packetID, _fileContents)
-    serverSock.sendto(_packet, clientAddr)
+    sock.sendto(_packet, clientAddr)
 
     #Stops and waits for acknowledgement of packet just sent
-    #Resends packet if incorrect ID acknowledgement is recieved,
-    #or if a timeout occurs.
-    while 1:
+    #Resends packet if incorrect ID acknowledgement is recieved
+    print('boopserver')
 
-        #Checks and counts timeouts
-        while 1:
-            try:
-                _ACK = serverSock.recv(2)
-                timeouts = 0
-                break
-            except:
-                if timeouts >= 4:
-                    print('Client unresponsive, terminating.')
-                    sys.exit(1)
-                serverSock.sendto(_packet, clientAddr)
-                print('Timeout...')
-                timeouts += 1
 
-        ACK = struct.unpack('H', _ACK)[0]
-        print(str(ACK))
-        print(str(packetID))
+def checkAck(sock):
+    global packetID
+    global _packet
+    global clientAddr
 
-        #Checks if the proper packet was acknowledged, if not
-        #server waits for next acknowledgement
-        if ACK == packetID:
-            wrongPacket = 0
-            break
+    _ACK = sock.recv(2)
+    ACK = struct.unpack('H', _ACK)[0]
+    print(str(ACK))
+    print(str(packetID))
+    if ACK == packetID:
+        packetID += 1
+        return True
+    return False
 
-        #Terminates after the fourth incorrect acknowledgement
-        if wrongPacket >= 4:
-            print('Network malfunction, terminating.')
+def resendPacket(sock):
+    global _packet
+    global clientAddr
+    sock.sendto(_packet, clientAddr)
+
+
+readSockFunc = {}               # ready for reading
+writeSockFunc = {}              # ready for writing
+errorSockFunc = {}
+
+readSockFunc[serverSock] = sendNextPacket
+
+while 1:
+    print('boopwhile')
+    readRdySet, writeRdySet, errorRdySet = select(list(readSockFunc.keys()),
+                                                list(writeSockFunc.keys()),
+                                                list(errorSockFunc.keys()),
+                                                timeout)
+
+    if not readRdySet and not writeRdySet and not errorRdySet:
+        print('timeout...')
+        timeouts += 1
+        if timeouts >= 4:
+            print('Client unresponsive, terminating')
+            if fileName: file.close()
             sys.exit(1)
-        #Packet is resent so client can acknowledge
-        serverSock.sendto(_packet, clientAddr)
-        wrongPacket += 1
 
-    #Stops sending packets after end of file is reached.
-    #FIX: SERVER NEEDS TO WAIT FOR LAST ACK
-    if not fileContents: break
-    packetID += 1
-
-serverSock.sendto(eof.encode(), clientAddr)
-
-file.close()
-
-print('File sent')
+    for sock in readRdySet:
+        print('boopfor')
+        if fileName == '':
+            openFile(sock)
+            sendNextPacket(sock)
+        elif checkAck(sock) == True:
+            sendNextPacket(sock)
+        else:
+            resendPacket(sock)
